4cc2183a63cd8a1a2fa914aa4367f2b654d9120b
tarquinn81 <qkarpowitz@gmail.com>
2013-02-23 23:30:50 -0700

    Finishing class readmap

--- Finding_that_object/src/readwrite/Readmap.java
+++ Finding_that_object/src/readwrite/Readmap.java
@@ -1,18 +1,26 @@
 import java.util.*;
 import java.io.*;
 package readwrite;
-class Readmap
+class Readmap //Reads the map file.
 {
-    public static void Character[][] Readmap ( String mapsave )throws IOExeption
+    public static void Readmap ( String mapsave )throws IOExeption
     {
-	int num1, row1, column1;
+	int num1;
+	int xmapsize = 10;
+	int row1 = 0;
+	int column1 = 0;
 	int nummap[row1][column1];
 	File map = new File( mapsave );
 	Scanner mapreader = new Scanner( map );
 	while ( mapreader.hasNextInt() )
 	{
 	    num1 = mapreader.nextInt();
-	    
+	    row1 +=;
+	    nummap[row1][column1] = num1;
+	    if ( row1 == xmapsize )
+	    {
+		column1 +=;
+	    }
 	}
     }
 }
